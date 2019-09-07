5a64488d22be6deb4e271df54e707873bff9eaf6
Tusgavin <dias.gdo@gmail.com>
2019-09-07 10:38:33 -0300

    react

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,1 +1,25 @@
-import React,{ Component } from 'react';
+import React, { Component } from 'react';
+
+class App extends Component{
+    state = { walletInfo: { address: 'foo-xv6', balance: 9999 } };
+
+    render() {
+        const  { address, balance } = this.state.walletInfo;
+
+        return(
+            <div>
+                <div>
+                    Welcome to my Cryptochain ! 
+                </div>
+                <div>
+                    Address: {address}
+                </div>
+                <div>
+                    Balance: {balance}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
